07e006ee744eadc0102420fe56e173b2a3f9d51f
BIT Student <classroom@bgit.rs>
2018-02-05 11:36:01 +0100

    promena

---
+++ switch/switch6.js
@@ -0,0 +1,23 @@
+var x = "B";
+
+switch (x) {
+    case "A":
+        console.log("Good job");
+        break;
+    case "B":
+        console.log("Okej job");
+        break;
+    case "C":
+        console.log("Passed");
+        break;
+    case "D":
+        console.log("Not so good");
+        break;
+    case "F":
+        console.log("Failed");
+        break;
+
+    default:
+    console.log("Unknown grade.");
+        break;
+}
