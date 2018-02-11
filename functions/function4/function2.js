278d01451028975d02064204fad5bdb1b42114e7
SavaJankovic <savudin92vb@gmail.com>
2018-02-11 15:47:04 +0100

    finish

---
+++ functions/function4/function2.js
@@ -0,0 +1,20 @@
+/*Write a program that multiplies 
+every positive element of a given array by 2. */
+
+function multi(arr) {
+    var newArr = [];
+    var counter = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            newArr[counter] = arr[i] * 2;
+            counter++;
+        } else if (arr[i] <= 0) {
+            newArr[counter] = arr[i];
+            counter++;
+        }
+    }
+    return newArr;
+}
+console.log(multi([2, 3, 4.5, -3, 0]));
+
---
+++ functions/function4/function3.js
@@ -0,0 +1,16 @@
+/*Write a program that finds the minimum of a 
+given array and prints out its value and index */
+
+function min(arr) {
+    var minNum = arr[0];
+    var indexMinNum;
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < minNum) {
+            minNum = arr[i];
+            indexMinNum = i;
+        }
+    }
+
+    return minNum + " > his index is " + indexMinNum;
+}
+console.log(min([2, 3, 4, -2, 5, 6, -7]));
