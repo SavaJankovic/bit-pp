3023c4bf84229f2ae5e82d8814be03823fa1ef83
SavaJankovic <savudin92vb@gmail.com>
2018-02-11 13:17:15 +0100

    finish

---
+++ functions/function4/function1.js
@@ -0,0 +1,18 @@
+/*Write a program that checks if a given element e is in
+the array a. 
+Input:  e = 3, a = [5, -4.2, 3, 7]
+Output: yes
+
+Input:  e = 3, a = [5, -4.2, 18, 7]
+Output: no */
+
+function checks(a, e) {
+    if (a.includes(e)) {
+        return "Yes";
+    } else {
+        return "No";
+
+    }
+}
+
+console.log(checks([5, -4.2, 3, 7], 13));
