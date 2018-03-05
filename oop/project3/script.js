4a074d93349e7ba6788fc554b36a1b3fd61b214b
unknown <savudin92vb@gmail.com>
2018-03-05 22:47:00 +0100

    progress

--- oop/project3/script.js
+++ oop/project3/script.js
@@ -1,13 +1,14 @@
 var dataModule = (function () {
 
     var data = {
-        movieList: []
+        movieList: [],
+        programList: []
     };
 
     function Movie(title, genre, length) {
         this.title = title;
         this.genre = genre;
-        this.length = length;
+        this.length = parseInt(length);
     }
 
 
@@ -17,14 +18,67 @@ var dataModule = (function () {
         return movie;
     }
 
+    function calculateMovieLength() {
+        var duration = data.movieList;
+        var count = 0;
+
+        duration.forEach(function (element) {
+            count += element.length;
+        });
+
+        return count;
+    }
+
 
     Movie.prototype.getData = function () {
-        return "Title: " + this.title + " Genre: " + this.genre + " Duration: " + this.length + " min";
+        return "Title: " + this.title + ", Genre: " + this.genre + ", Duration: " + this.length + " min";
 
     };
 
+    function Program(date) {
+        this.date = new Date(date);
+        this.movies = [];
+        this.numMovies = 0;
+        this.programDuration = 0;
+    }
+
+    Program.prototype.getData = function () {
+        var datum = this.date.getDate() + "/" + (this.date.getMonth() + 1) + "/" + this.date.getFullYear();
+        var totalDuration = 0;
+
+        this.movies.forEach(function (elem) {
+            totalDuration += elem.length;
+        })
+
+
+        return datum + ", movies length : " + this.movies.length + ", duration of movies:  " + totalDuration + " min";
+    }
+
+    // Program.prototype.getNumOfMovies = function () {
+    //     return this.movies.length;
+    // }
+
+    // Program.prototype.getMoviesDuration = function () {
+    //     var totalDuration = 0;
+    //     this.movies.forEach(function (elem) {
+    //         totalDuration += elem.length;
+    //     })
+    //     return totalDuration;
+    // }
+
+    function addProgramToList(programD) {
+        var program = new Program(programD.dateP);
+        data.programList.push(program);
+        return program;
+
+    }
+
+
+
     return {
-        addM: addMovieToList
+        addM: addMovieToList,
+        calcDuration: calculateMovieLength,
+        addP: addProgramToList
     }
 
 })();
@@ -37,11 +91,16 @@ var uiModule = (function () {
         var select = document.querySelector("#genreMovie");
         var genre = select.options[select.selectedIndex].text;
 
+
+
+
         return {
             titleMovie: title,
             lengthMovie: length,
             genreMovie: genre
         }
+
+
 
     };
 
@@ -54,11 +113,71 @@ var uiModule = (function () {
         var textMovie = document.createTextNode(movie.getData());
         liMovie.appendChild(textMovie);
         ulMovie.appendChild(liMovie);
+
+        // DA LI TREBA OVAKO
+        var addingMovie = document.querySelector("#finalMovie");
+        var optionMovie = document.createElement("option");
+        // optionMovie.value = data.movieList.length - 1;
+        addingMovie.appendChild(optionMovie);
+
+        var nameOfMovie = document.createTextNode(movie.getData());
+
+        optionMovie.appendChild(nameOfMovie);
+
     }
+
+
+    function showMovieDuration(calcDur) {
+        var divMovie = document.querySelector("#newDuration");
+        var ulMovie = document.createElement("ul");
+        divMovie.appendChild(ulMovie);
+        var liMovie = document.createElement("li");
+        var textMovie = document.createTextNode("Movies length: " + calcDur + " min.");
+        liMovie.appendChild(textMovie);
+        ulMovie.appendChild(liMovie);
+
+
+    }
+
+
+    function getProgramData() {
+        var dateProgram = document.querySelector("#dateProgram").value;
+
+        return {
+            dateP: dateProgram
+        }
+
+    }
+
+    function showProgramData(proDate) {
+        var divMovie = document.querySelector("#newProgram");
+        var ulMovie = document.createElement("ul");
+        divMovie.appendChild(ulMovie);
+        var liMovie = document.createElement("li");
+        var textMovie = document.createTextNode(proDate.getData());
+        liMovie.appendChild(textMovie);
+        ulMovie.appendChild(liMovie);
+
+
+        //   DA LI TREBA OVAKO?
+
+        var addingProgram = document.querySelector("#finalProgram");
+        var optionProgram = document.createElement("option");
+        // optionProgram.value = listOfMovie.length - 1;
+        addingProgram.appendChild(optionProgram);
+        var dateOfProgram1 = document.createTextNode(proDate.getData());
+        var dateProgram = document.createTextNode(dateOfProgram1);
+
+        optionProgram.appendChild(dateOfProgram1);
+    }
+
 
     return {
         movieDataForm: getFormData,
-        showMovie: showMovieData
+        showMovie: showMovieData,
+        showDuration: showMovieDuration,
+        datum: getProgramData,
+        showProgram: showProgramData
     }
 
 
@@ -72,12 +191,22 @@ var contrModule = (function (dModule, uiModule) {
         var dataMovie = uiModule.movieDataForm();
         var movie = dModule.addM(dataMovie);
 
-
+        var calcDur = dataModule.calcDuration();
+        uiModule.showDuration(calcDur);
 
         uiModule.showMovie(movie);
 
 
-        console.log(dataMovie);
+
+
+    });
+
+    document.querySelector(".createProgram").addEventListener("click", function () {
+        var programD = uiModule.datum();
+        var proDate = dModule.addP(programD);
+        uiModule.showProgram(proDate);
+
+        console.log(proDate);
     });
 
 
